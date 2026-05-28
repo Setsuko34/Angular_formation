50bbc3898ad414fed8b0f37e66f60c7f91d7cb26
MFuline-ext <mfuline-ext@ugap.fr>
2026-05-28 10:11:48 +0200

    refactor login component; enhance UI with animations and validation; update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import {Welcome} from './welcome/welcome';
 import {Basic} from './basic/basic';
-import {Login} from './login/login';
+import {LoginComponent} from './login/login';
 
 export const routes: Routes = [
   { path: 'ngr-welcome', component: Welcome },
   { path: '', redirectTo: '/ngr-welcome', pathMatch: 'full'},
-  { path: 'ngr-login', component: Login },
+  { path: 'ngr-login', component: LoginComponent },
   { path: 'ngr-basic', component: Basic },
   { path: '**', redirectTo: '/ngr-welcome', pathMatch: 'full'}
 ];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {Login} from '../common/data/login';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class LoginComponent {
+  public login : Login = new Login();
+  public message = "";
+  public onLogin(){
+    this.message = `Bienvenue ${this.login.username} ! Vos rôles sont : ${this.login.roles}`;
+  }
+}
